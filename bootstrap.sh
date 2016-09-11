a59b95dc3e4214248def5de30acc84a9d3f44c3f
Yu Nishikawa <y.nishi0320@gmail.com>
2016-09-11 10:02:01 +0900

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,14 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
 echo "Clone dotfiles repository..."
 git clone --recursive https://github.com/nishik20/dotfiles.git $HOME/.dotfiles
 
 echo "Symlink dotfiles..."
-for f in $(find $HOME/.dotfiles -name ".*" -maxdepth 1); do
+pushd $HOME/.dotfiles
+for f in $(find . -name ".*" -maxdepth 1); do
     name=$(echo $f | sed -e 's|\.\/||')
-    if [ $name != "." -o $name != ".git" ]; then
+    if [ $name != "." ] && [ $name != ".git" ]; then
         ln -sf $HOME/.dotfiles/$name $HOME/$name
     fi
 done
+popd
